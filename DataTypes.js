5eda883ef5a4f0dc947364840fd7bddb6e68dbea
Rakesh-1143 <rakeshreddy261002@gmail.com>
2025-01-04 23:43:56 +0530

    All predefined datatypes

---
+++ DataTypes.js
@@ -0,0 +1,21 @@
+//number data type
+var a= 10
+console.log(a,typeof a)
+
+//String
+
+var b='Rakesh'
+console.log(b,typeof b)
+
+//Boolean
+
+var c=false;
+console.log(c,typeof c)
+
+//undefined:
+let d;
+console.log(d)
+
+//null
+let e=null;
+console.log(e)
