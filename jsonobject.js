6a1e855843f236d0b56b4bc2fc71a3d090763783
Rakesh-1143 <rakeshreddy261002@gmail.com>
2025-01-05 07:46:06 +0530

    Jsonobject intro

---
+++ jsonobject.js
@@ -0,0 +1,15 @@
+const s1={
+    Name:'rakesh',
+    Age:24
+};
+ const s2={
+    Name:"Ravi",
+    Email:"Ravi@gmail.com"
+ };
+ const s3 = {
+    Name: 'Krishna',
+    Work_experience: {
+        xyz: '3 Months',
+        abc: '12 Months'
+    }
+};
