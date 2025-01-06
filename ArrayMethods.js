7301068ad5215022280cb3691821dbcad806aea9
Rakesh-1143 <rakeshreddy261002@gmail.com>
2025-01-06 11:52:29 +0530

    sample program

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -8,6 +8,15 @@ for (var i=0;i<=arr1.length;i++){
 var arr2=[10,20,30]
 arr2.push(30)
 console.log("After push",arr2)
-
+//insert at frist position
 arr2.unshift(5)
 console.log("After unshift",arr2)
+
+arr2.pop()
+console.log(arr2)
+arr2.shift()
+console.log(arr2)
+
+console.log(arr2.includes(30))
+
+console.log(arr2.indexOf(30))
---
+++ newArrMethods.js
@@ -0,0 +1,14 @@
+var arr1=[10,20,30,40]
+var arr2=[]
+
+for (var i=0;i<arr1.length;i++){
+    arr2[i]=arr1[i]+5
+}
+console.log(arr2)
+
+// var arr1=[10,20,30,40]
+// var arr2=[]
+// for(var i=0;i<arr1.length;i++){
+//     arr2.push(arr1[i]+5)
+// }
+// console.log(arr2)
