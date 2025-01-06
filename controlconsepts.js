da309b093cccdeb7a6e92838872851a20fd20c4d
Rakesh-1143 <rakeshreddy261002@gmail.com>
2025-01-06 23:28:13 +0530

    looping and function

---
+++ controlconsepts.js
@@ -0,0 +1,42 @@
+//if -else
+// var age=19;
+// if (age>19){
+//     console.log("Your age is graterthan18")
+// }
+// else{
+//     console.log("Age is less then the 18")
+// }
+
+// let day="Tuesday"
+// switch(day){
+//     case "Monday":
+//         console.log("This is monday")
+//     case "Tuesday":
+//         console.log("this is thuesday")
+//     default:
+//         console.log("defaultvalues")
+// }
+
+//loopings
+// let count=1
+// while (count<=5){
+//     console.log("count is;",count)
+        // count++
+// }
+
+// do while is present in js
+
+//for of loop-->numbers of and in for strings
+// var arr=[100,200,300]
+// for (var i of arr){
+//     console.log(i)
+// }
+
+//for Each
+// var arr3=[10,20,30]
+// arr3.forEach((ele)=>{
+//     console.log(ele)
+// })
+
+//jumping statements in javascript
+//1.break,continue
--- newArrMethods.js
+++ newArrMethods.js
@@ -1,14 +1,48 @@
-var arr1=[10,20,30,40]
-var arr2=[]
-
-for (var i=0;i<arr1.length;i++){
-    arr2[i]=arr1[i]+5
-}
-console.log(arr2)
-
 // var arr1=[10,20,30,40]
 // var arr2=[]
-// for(var i=0;i<arr1.length;i++){
-//     arr2.push(arr1[i]+5)
+
+// for (var i=0;i<arr1.length;i++){
+//     arr2[i]=arr1[i]+5
 // }
 // console.log(arr2)
+
+
+
+//filter map and reduce methods in js
+// arr1=[10,11,12,13,4,15]
+// var newarr=arr1.filter(function(ele){
+//     if (ele%2===0){
+//         return ele
+//     }
+// })
+// console.log("orginal Array",arr1)
+// console.log("Even array:",newarr)
+
+// var years=[1996,2000,1997,1994]
+// var leapyear=years.filter(function(ele){
+//     if ((ele%4===0 && ele%100!==0) || (ele%400===0)){
+//         return ele
+//     }
+// })
+
+// console.log("Leap Years:", leapyear);
+
+// var array =[1,2,3,4,5,6]
+// var sq_arr=array.map(function(ele){
+//     return ele*ele
+// })
+// console.log(sq_arr)
+
+// var array=[1,2,3,4,5,6]
+// var sq_arr= array.map((ele)=>ele*ele)
+// console.log(sq_arr)
+
+// var arr=[1,2,3,4,5]
+// var sum=arr.reduce((acc,ele)=>acc+ele,0)
+// console.log(sum)
+
+
+//working of forEachloop
+
+var array=[1,2,3,4]
+array.forEach((ele,idx,array)=>console.log(ele,idx,array))
